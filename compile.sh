39be0b95e861ebee47e72fa68d9c99b08fde3a4c
Maximilian Weigand <mweigand@mweigand.net>
2023-06-01 17:11:06 +0200

    test ci matrix 1

--- compile.sh
+++ compile.sh
@@ -2,18 +2,18 @@
 env
 cd /root/mutter
 
-if [ ! -d mutter ]; then
-    git clone --branch debian/master https://salsa.debian.org/gnome-team/mutter.git
-    cd mutter
-    patch -p1 < ../0001-Add-META_CONNECTOR_TYPE_DPI.patch
-    cd ..
-fi
+# if [ ! -d mutter ]; then
+#     git clone --branch debian/master https://salsa.debian.org/gnome-team/mutter.git
+#     cd mutter
+#     patch -p1 < ../0001-Add-META_CONNECTOR_TYPE_DPI.patch
+#     cd ..
+# fi
 
-cd mutter
-time DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage --build=binary
-cd ..
+# cd mutter
+# time DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage --build=binary
+# cd ..
 
-test -d mutter_arm64_debs && rm -r mutter_arm64_debs
+# test -d mutter_arm64_debs && rm -r mutter_arm64_debs
 mkdir mutter_arm64_debs
 echo "huhu" >  mutter_arm64_debs/asdasd.txt
 mv *.deb mutter_arm64_debs/
